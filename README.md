d4a15a2fce4204cde00034dda2cde01e6741ac5c
MackZhong <mackz@sina.com>
2022-12-29 10:23:02 +0800

    Initialization functions

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Console tank game uses ncurses on Linux, for C learning.
 1. install ncurses library.
 2. #include <ncurses.h>
 3. link with ncurses library.
+4. In CMakeLists.txt file, use `include(FindCurses)` to find curses library and `target_link_library(appname ncurses)` to link the library.
+5. In VSCode, when Debug run use F5 at external terminal, it should add environment variable `$TERM` into 'launch.json' as below:
+    "env": { "TERM": "xterm" }
+  the terminal type is as your wish. If not do so, the external terminal is null, and ncurses initscr() call will fail.
+6. When launch the program from run icon on the statusbar use clangd, it always run in integrated terminal.
--- main.c
+++ main.c
@@ -4,11 +4,41 @@
 // ncurses terminal game
 int main(void) {
 
-  initscr();                   /* Start curses mode 		  */
-  printw("Hello ncurses !!!"); /* Print Hello World		  */
-  refresh();                   /* Print it on to the real screen */
-  getch();                     /* Wait for user input */
-  endwin();                    /* End curses mode		  */
+  initscr(); /* Start curses mode 		  */
+
+  raw(); /* Line buffering disabled. 	*/
+  /* control characters: suspend (CTRL-Z), interrupt and quit (CTRL-C).
+    In the raw() mode these characters are directly passed to the program
+    without generating a signal. In the cbreak() mode these control characters
+    are interpreted as any other character by the terminal driver. */
+  keypad(stdscr, TRUE); /* We get F1, F2 and arrow keys etc..		*/
+  noecho();             /* Don't echo() while we do getch() call. */
+
+  //   halfdelay(1);
+  /* Similar to the cbreak() mode in that characters typed are
+    immediately available to program. However, it waits for 'X'
+    tenths of a second for input and then returns ERR, if no
+    input is available.  */
+
+  printw("Type any character to see it in bold\n");
+  int ch = getch();           /* If raw() hadn't been called
+                               * we have to press enter before it
+                               * gets to the program 		*/
+  if (ch == KEY_F(1))         /* Without keypad enabled this will */
+    printw("F1 Key pressed"); /*  not get to us either	*/
+                              /* Without noecho() some ugly escape
+                               * charachters might have been printed
+                               * on screen			*/
+  else {
+    printw("The pressed key is ");
+    attron(A_BOLD);
+    printw("%c", ch);
+    attroff(A_BOLD);
+  }
+
+  refresh(); /* Print it on to the real screen */
+  getch();   /* Wait for user input */
+  endwin();  /* End curses mode		  */
 
   return 0;
 }
