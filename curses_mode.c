e99cd5af8348d9472dc8e06514238ae435953573
MackZhong <mackz@sina.com>
2023-01-09 13:42:34 +0800

    Sloved the problem why curses screen lost.

--- curses_mode.c
+++ curses_mode.c
@@ -5,42 +5,56 @@
 int main() {
   setlocale(LC_ALL, "C");
 
-  WINDOW *winMain = initscr(); /* Start curses mode 		  */
-  if (NULL == winMain) {
+  WINDOW *win_main = initscr(); /* Start curses mode 		  */
+  if (NULL == win_main) {
     printf("Init curses failed.\n");
     return -1;
   }
   printf("Curses initialized.\n");
 
-  if (winMain != stdscr) {
+  if (win_main != stdscr) {
     printf("Screen initialization error.\n");
     endwin();
     return -2;
   }
 
-  // clear();
-  // noecho();
-  // cbreak(); // Line buffering disabled. pass on everything
+  clear();
+  noecho();
+  cbreak(); // Line buffering disabled. pass on everything
   addstr("Curses started.\n");
-  printw("Hello Curses !!!\n"); /* Print Hello World		  */
-  refresh();                    /* Print it on to the real screen */
+  printw("Input 's' to cooked mode, 'q' to quit.\n"); /* Print Hello World
+                                                       */
+  refresh(); /* Print it on to the real screen */
 
-  // get back to cooked mode (normal line buffering mode) temporarily.
-  // def_prog_mode(); /* Save the tty modes		  */
-  // endwin();        /* End curses mode temporarily	  */
+  int chr = getch();
+  if ('s' == chr) {
+    // get back to cooked mode (normal line buffering mode) temporarily.
+    def_prog_mode(); /* Save the tty modes		  */
+    endwin();        /* End curses mode temporarily	  */
 
-  // system("/bin/sh"); /* Do whatever you like in cooked mode */
+    system("/bin/sh"); /* Do whatever you like in cooked mode */
 
-  // // To get back to curses mode
-  // reset_prog_mode(); /* Return to the previous tty mode*/
-  //                    /* stored by def_prog_mode() 	  */
-  // refresh();         /* Do refresh() to restore the	  */
-  /* Screen contents		  */
+    // To get back to curses mode
+    reset_prog_mode(); /* Return to the previous tty mode*/
+                       /* stored by def_prog_mode() 	  */
 
-  printw("Another String\n"); /* Back to curses use the full    */
-  refresh();                  /* capabilities of curses	  */
+    printw("Baock curses mode.\n"); /* Back to curses use the full    */
+
+    // refresh(); /* Do refresh() to restore the	  */
+    /* Screen contents		  */
+  } else if ('q' == chr) {
+    printw("EXIT.\n");
+  }
+
+  refresh(); /* capabilities of curses	  */
+
+  printw("Press any key.\n");
+  getch();
+  /* The curses screen is not in the same console with the program, so it SHOULD
+   * add getch() before endwin() to keep the curses scheen visible!*/
 
   endwin(); /* End curses mode		  */
+  printf("Exit curses.\n");
 
   return 0;
 }
